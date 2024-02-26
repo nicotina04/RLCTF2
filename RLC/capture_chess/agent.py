abb8a45721b61dee6d57c92e1f8ccd6a0b02938b
HOGYEONG CHOE <devherdson@gmail.com>
2024-02-26 12:53:12 +0000

    Fix: Change SGD argument
    Change lr to learning_rate

--- RLC/capture_chess/agent.py
+++ RLC/capture_chess/agent.py
@@ -52,7 +52,7 @@ class Agent(object):
         The fixed model is the model used for bootstrapping
         Returns:
         """
-        optimizer = SGD(lr=self.lr, momentum=0.0, decay=0.0, nesterov=False)
+        optimizer = SGD(learning_rate=self.lr, momentum=0.0, decay=0.0, nesterov=False)
         self.fixed_model = clone_model(self.model)
         self.fixed_model.compile(optimizer=optimizer, loss='mse', metrics=['mae'])
         self.fixed_model.set_weights(self.model.get_weights())
@@ -63,7 +63,7 @@ class Agent(object):
         Returns:
 
         """
-        optimizer = SGD(lr=self.lr, momentum=0.0, decay=0.0, nesterov=False)
+        optimizer = SGD(learning_rate=self.lr, momentum=0.0, decay=0.0, nesterov=False)
         input_layer = Input(shape=(8, 8, 8), name='board_layer')
         reshape_input = Reshape((512,))(input_layer)
         output_layer = Dense(4096)(reshape_input)
@@ -76,7 +76,7 @@ class Agent(object):
         Returns:
 
         """
-        optimizer = SGD(lr=self.lr, momentum=0.0, decay=0.0, nesterov=False)
+        optimizer = SGD(learning_rate=self.lr, momentum=0.0, decay=0.0, nesterov=False)
         input_layer = Input(shape=(8, 8, 8), name='board_layer')
         inter_layer_1 = Conv2D(1, (1, 1), data_format="channels_first")(input_layer)  # 1,8,8
         inter_layer_2 = Conv2D(1, (1, 1), data_format="channels_first")(input_layer)  # 1,8,8
@@ -93,7 +93,7 @@ class Agent(object):
         Returns:
 
         """
-        optimizer = SGD(lr=self.lr, momentum=0.0, decay=0.0, nesterov=False)
+        optimizer = SGD(learning_rate=self.lr, momentum=0.0, decay=0.0, nesterov=False)
         input_layer = Input(shape=(8, 8, 8), name='board_layer')
         R = Input(shape=(1,), name='Rewards')
         legal_moves = Input(shape=(4096,), name='legal_move_mask')
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='RLC',
-    version='0.3',
+    name='RLCTF2',
+    version='0.31',
     packages=['RLC', 'RLC.move_chess', 'RLC.capture_chess', 'RLC.real_chess'],
-    url='https://github.com/arjangroen/RLC',
+    url='https://github.com/nicotina04/RLCTF2',
     license='MIT',
-    author='a.groen',
-    author_email='arjanmartengroen@gmail.com',
-    description='Collection of reinforcement learning algorithms, all applied to chess or chess related problems'
+    author='nicotina04',
+    author_email='devherdson@gmail.com',
+    description='Collection of reinforcement learning algorithms, all applied to chess or chess related problems. This repository supports Tensorflow2'
 )
